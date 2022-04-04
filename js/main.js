0abf6a546c4aba0e0eb0e77eae5f2eb0f6ccd8b1
Roger Davila <rogerddavila@gmail.com>
2022-04-04 15:49:25 -0700

    create solitaire object with modular sub classes and render card elements

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
+import CardPile from './CardPile.js';
+import TableauPile from './TableauPile.js';
 import Deck from './Deck.js';
 import Tableau from './Tableau.js';
 import Foundation from './Foundation.js';
 import Solitaire from './Solitaire.js';
 
-const mainEl = document.querySelector('main');
 const tableauColumns = [...document.querySelectorAll('.column')];
 const handEl = document.querySelector('#hand');
-console.log(mainEl);
 console.log(tableauColumns);
+
+const foundationPiles = Array(4).fill(null).map(_ => new CardPile());
+const foundation = new Foundation(...foundationPiles);
+
+const tableauPiles = Array(7).fill(null).map(_ => new TableauPile())
+const tableau = new Tableau(...tableauPiles);
+
+const waste = new CardPile();
+
+const deck = new Deck();
+deck.shuffle();
 
 console.log(Deck.suits);
 console.log(Deck.ranks);
 console.log(Deck.valuesTable);
 
-const deck = new Deck();
-deck.shuffle();
 
-const solitaire = new Solitaire(deck, new Foundation(), new Tableau());
+
+const solitaire = new Solitaire(deck, waste, foundation, tableau);
 solitaire.tableau.setTableau(solitaire.hand); // Use the hand pile to distribute card to the tableau
 console.log(solitaire);
 
@@ -25,15 +35,15 @@ renderTableau();
 
 function renderDeck() {
     solitaire.hand.cards.forEach((card) => {
-    let cardEl = document.createElement('div');
-    cardEl.setAttribute('class', card.faceup ? `card ${card.suit}${card.shorthand} shadow deck` : `card back-blue shadow deck`);
-    handEl.append(cardEl);
+        let cardEl = document.createElement('div');
+        cardEl.setAttribute('class', card.faceup ? `card ${card.suit}${card.shorthand} shadow deck` : `card back-blue shadow deck`);
+        handEl.append(cardEl);
     });
 }
 
 function renderTableau() {
     tableauColumns.forEach((tableauColumn, idx) => {
-        solitaire.tableau[idx].forEach((card) => {
+        solitaire.tableau[idx].cards.forEach((card) => {
             let cardEl = document.createElement('div');
             cardEl.setAttribute('class', card.faceup ? `card ${card.suit}${card.shorthand} shadow` : `card back-blue shadow`);
             tableauColumn.append(cardEl);
