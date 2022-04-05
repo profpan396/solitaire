0b1395a4486de5bac9fcc2caf5fd166799ef940b
Roger Davila <rogerddavila@gmail.com>
2022-04-04 22:42:00 -0700

    add waste render function

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@
 
 const tableauColumns = [...document.querySelectorAll('.column')];
 const handEl = document.querySelector('#hand');
+const wasteEl = document.querySelector('#waste');
 console.log(tableauColumns);
 
 const foundationPiles = Array(4).fill(null).map(_ => new CardPile());
@@ -18,14 +19,18 @@ const tableau = new Tableau(...tableauPiles);
 const waste = new CardPile();
 
 const deck = new Deck();
+console.log(deck);
 deck.shuffle();
+console.log(deck);
 
 const solitaire = new Solitaire(deck, waste, foundation, tableau);
+console.log(solitaire);
 solitaire.tableau.setTableau(solitaire.hand); // Use the hand pile to distribute card to the tableau
 console.log(solitaire);
 
 renderDeck();
 renderTableau();
+renderWaste();
 
 function renderDeck() {
     solitaire.hand.cards.forEach((card) => {
@@ -45,4 +50,13 @@ function renderTableau() {
         });
     });
     console.log(solitaire.tableau);
+}
+
+function renderWaste() {
+    wasteEl.innerHTML = '';
+    solitaire.waste.cards.forEach((card) => {
+        let cardEl = document.createElement('div');
+        cardEl.setAttribute('class', card.faceup ? `card ${card.suit}${card.shorthand} shadow` : `card back-blue shadow`);
+        wasteEl.append(cardEl);
+    });
 }
