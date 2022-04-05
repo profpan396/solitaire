a17181085b85806c571b18c41634583eda29e90e
Roger Davila <rogerddavila@gmail.com>
2022-04-05 10:37:26 -0700

    add reset to waste pile after reseting hand

--- js/Solitaire.js
+++ js/Solitaire.js
@@ -4,18 +4,20 @@ class Solitaire {
         this.waste = waste;
         this.foundation = foundation;
         this.tableau = tableau;
-        this.cardsToDraw = -1;
+        this.cardsToDraw = -3;
     }
     resetHand() {
         this.waste.cards.reverse();
-        this.waste.cards.forEach((card) => {
+        this.waste.cards.forEach((card, idx) => {
+            console.log(`${idx} - Card: ${card}`);
             if(card.faceup) card.flip();
             this.hand.addCardToPile(card);
         });
+        this.waste.cards = [];
     }
     draw() {
-        this.hand.grabCardsFromPosition(this.cardsToDraw).forEach((card) => {
-            console.log(card);
+        this.hand.grabCardsFromPosition(this.cardsToDraw).forEach((card, idx) => {
+            console.log(`${idx} - Card: ${card}`);
             card.flip();
             this.waste.addCardToPile(card);
         });
