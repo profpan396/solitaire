d5d3d0fdb3d2efd81d6a85af1df3eef348b8d653
Roger Davila <rogerddavila@gmail.com>
2022-04-04 22:35:36 -0700

    add hand reset method and draw method

--- js/Solitaire.js
+++ js/Solitaire.js
@@ -4,5 +4,20 @@ class Solitaire {
         this.waste = waste;
         this.foundation = foundation;
         this.tableau = tableau;
+        this.cardsToDraw = -1;
+    }
+    resetHand() {
+        this.waste.cards.reverse();
+        this.waste.cards.forEach((card) => {
+            if(card.faceup) card.flip();
+            this.hand.addCardToPile(card);
+        });
+    }
+    draw() {
+        this.hand.grabCardsFromPosition(this.cardsToDraw).forEach((card) => {
+            console.log(card);
+            card.flip();
+            this.waste.addCardToPile(card);
+        });
     }
 }
