dd08c37a644e6b9a85a8a4940b33082245dd5f58
Roger Davila <rogerddavila@gmail.com>
2022-04-04 21:05:06 -0700

    remove grab card method

--- js/Tableau.js
+++ js/Tableau.js
@@ -2,9 +2,6 @@ class Tableau {
     constructor(...tableauPiles) {
         tableauPiles.forEach((tableauPile, i) => this[i] = tableauPile);
     }
-    grabAllFromPosition(n, pile) {
-        return this[pile].splice(n, this[pile].length - 1);
-    }
     setTableau(deck) {
         Object.keys(this).forEach((pileKey) => {
             for (let i = 0; i < parseInt(pileKey) + 1; i++) {
