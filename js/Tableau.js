d7aa1b0b91a40206c433baffd3543275f57166c5
Roger Davila <rogerddavila@gmail.com>
2022-04-06 11:26:16 -0700

    add cardIsOpposite method

--- js/Tableau.js
+++ js/Tableau.js
@@ -11,4 +11,14 @@ class Tableau {
             }
         });
     }
+    cardIsOpposite(cardOne, cardTwo) {
+        return cardOne.isRed() !== cardTwo.isRed();
+    }
+    isPlaceable(card) {
+        Object.keys(this).forEach((tKey) => {
+            console.log(this[tKey]);
+            console.log(this[tKey].cards.slice(-1)[0].value === card.value + 1 && this[tKey].cards.slice(-1)[0]);
+        });
+        // return this[card.suit].cards.length === 0 ? card.value === 1 : card.value === this[card.suit].cards.slice(-1)[0].value + 1;
+    }
 }
