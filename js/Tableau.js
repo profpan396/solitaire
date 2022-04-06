410860c4fb430858513dddfb435576b3c6eb97d9
Roger Davila <rogerddavila@gmail.com>
2022-04-06 13:24:36 -0700

    test method if card is placeable

--- js/Tableau.js
+++ js/Tableau.js
@@ -14,10 +14,18 @@ class Tableau {
     cardIsOpposite(cardOne, cardTwo) {
         return cardOne.isRed() !== cardTwo.isRed();
     }
+    cardIsLessThan(cardOne, cardTwo) {
+        return cardOne.value === cardTwo.value -1;
+    }
     isPlaceable(card) {
         Object.keys(this).forEach((tKey) => {
             console.log(this[tKey]);
-            console.log(this[tKey].cards.slice(-1)[0].value === card.value + 1 && this[tKey].cards.slice(-1)[0]);
+            console.log(card);
+            console.log(this[tKey].cards.slice(-1)[0]);
+            console.log(`Opposite Color: ${this.cardIsOpposite(card, this[tKey].cards.slice(-1)[0])}`);
+            console.log(`Less Than Pile: ${this.cardIsLessThan(card, this[tKey].cards.slice(-1)[0])}`);
+            console.log(`Placeable: ${this.cardIsOpposite(card, this[tKey].cards.slice(-1)[0]) && this.cardIsLessThan(card, this[tKey].cards.slice(-1)[0])}`)
+            // console.log(this[tKey].cards.slice(-1)[0].value === card.value + 1 && this[tKey].cards.slice(-1)[0]);
         });
         // return this[card.suit].cards.length === 0 ? card.value === 1 : card.value === this[card.suit].cards.slice(-1)[0].value + 1;
     }
