162eed52b576322fcb017bd507ecabbb03eb6cd6
Roger Davila <rogerddavila@gmail.com>
2022-04-07 13:55:34 -0700

    remove console logs and unnecessary place card method

--- js/Foundation.js
+++ js/Foundation.js
@@ -5,15 +5,10 @@ class Foundation {
         this.s = cardPiles[2];
         this.c = cardPiles[3];
     }
-    removeCardFromFoundation(card) {
-        this[card.suit].grabTopCard();
-    }
     addCardToFoundation(card) {
         this[card.suit].addCardToPile(card);
     }
     isPlaceableFoundation(card) {
-        console.log(this[card.suit]);
-        console.log(card.suit);
         return this[card.suit].cards.length === 0 ? card.value === 1 : card.value === this[card.suit].cards.slice(-1)[0].value + 1;
     }
 }
