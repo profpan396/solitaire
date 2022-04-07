3165837314e6d8edf5614ee60743a4b35eba9c66
Roger Davila <rogerddavila@gmail.com>
2022-04-07 13:46:02 -0700

    format code indentation

--- js/Deck.js
+++ js/Deck.js
@@ -1,7 +1,7 @@
 // import Card from './Card.js';
 // import CardPile from './CardPile.js';
 
-class Deck extends CardPile{
+class Deck extends CardPile {
     constructor() {
         super();
         Deck.suits.forEach((suit) => Deck.ranks.forEach((rank) => this.cards.push(new Card(suit, rank, Deck.valuesTable))));
@@ -23,22 +23,22 @@ class Deck extends CardPile{
 }
 
 // Add constants to Deck prototype
-Deck.suits = ['h','d','s','c'];
-Deck.ranks = ['ace','two','three','four','five','six','seven','eight','nine','ten','jack','queen','king'];
+Deck.suits = ['h', 'd', 's', 'c'];
+Deck.ranks = ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king'];
 Deck.valuesTable = {
-    ace: {value: 1, shorthand: 'A'}, 
-    two: {value: 2, shorthand: '02'},
-    three: {value: 3, shorthand: '03'},
-    four: {value: 4, shorthand: '04'},
-    five: {value: 5, shorthand: '05'},
-    six: {value: 6, shorthand: '06'},
-    seven: {value: 7, shorthand: '07'},
-    eight: {value: 8, shorthand: '08'},
-    nine: {value: 9, shorthand: '09'},
-    ten: {value: 10, shorthand: '10'},
-    jack: {value: 11, shorthand: 'J'},
-    queen: {value: 12, shorthand: 'Q'},
-    king: {value: 13, shorthand: 'K'},
+    ace: { value: 1, shorthand: 'A' },
+    two: { value: 2, shorthand: '02' },
+    three: { value: 3, shorthand: '03' },
+    four: { value: 4, shorthand: '04' },
+    five: { value: 5, shorthand: '05' },
+    six: { value: 6, shorthand: '06' },
+    seven: { value: 7, shorthand: '07' },
+    eight: { value: 8, shorthand: '08' },
+    nine: { value: 9, shorthand: '09' },
+    ten: { value: 10, shorthand: '10' },
+    jack: { value: 11, shorthand: 'J' },
+    queen: { value: 12, shorthand: 'Q' },
+    king: { value: 13, shorthand: 'K' },
 };
 
 
