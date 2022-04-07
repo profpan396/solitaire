97fbd1e245db734702defb7999be331550b8f637
Roger Davila <rogerddavila@gmail.com>
2022-04-07 14:47:26 -0700

    update card placement methods

--- js/CardPile.js
+++ js/CardPile.js
@@ -8,4 +8,10 @@ class CardPile {
     addCardToPile(card) {
         this.cards.push(card);
     }
+    grabCardsFromPosition(n) {
+        return this.cards.splice(n, this.cards.length);
+    }
+    addCardsToPile(cards) {
+        this.cards = this.cards.concat(cards);
+    }
 }
