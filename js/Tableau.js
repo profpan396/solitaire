05f4fd6e2a447f92484f585e404b3e13d7d96c2e
Roger Davila <rogerddavila@gmail.com>
2022-04-07 18:44:52 -0700

    fix loop function to prioritize first column

--- js/Tableau.js
+++ js/Tableau.js
@@ -19,11 +19,17 @@ class Tableau {
     }
     isPlaceableTableau(card) {
         let key;
-        Object.keys(this).forEach((tKey) => {
-            let cardTwo = this[tKey].cards.slice(-1)[0];
-            if (this[tKey].cards.length === 0 && card.rank !== 'king') return;
-            else if (this.cardIsOpposite(card, cardTwo) && this.cardIsLessThan(card, cardTwo)) key = tKey;
-        });
-        return key;
+        let tabIndices = Object.keys(this);
+        for (const tabIdx in tabIndices) {
+            let cardTwo = this[tabIdx].cards.slice(-1)[0];
+            if (this[tabIdx].cards.length === 0 && card.rank !== 'king') continue;
+            else if (this.cardIsOpposite(card, cardTwo) && this.cardIsLessThan(card, cardTwo)) return tabIdx;
+        }
+        // Object.keys(this).forEach((tKey) => {
+        //     let cardTwo = this[tKey].cards.slice(-1)[0];
+        //     if (this[tKey].cards.length === 0 && card.rank !== 'king') return;
+        //     else if (this.cardIsOpposite(card, cardTwo) && this.cardIsLessThan(card, cardTwo)) key = tKey;
+        // });
+        return false;
     }
 }
